bc5d833bc5727abfd6bcaadb82bca2b6acd46ae6
lancs <879294955@qq.com>
2020-07-03 15:03:02 +0800

    123

---
+++ git-test-project/src/com/github/test/Branch.java
@@ -0,0 +1,11 @@
+package com.github.test;
+
+/**
+ * @ClassName:git-test-project
+ * @Description:
+ * @author:lancs
+ * @date:2020-07-03 15:02
+ * @Copyright:2020 www.jinwang.com.cn Inc. All rights reserved.
+ */
+public class Branch {
+}
