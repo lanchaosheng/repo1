e5ed180aecd52ba17cd0ed92da1a4cc0b55b2a80
lancs <879294955@qq.com>
2020-07-03 14:44:06 +0800

    1

---
+++ git-test-project/src/com/github/test/Main.java
@@ -0,0 +1,14 @@
+package com.github.test;
+
+/**
+ * @ClassName:git-test-project
+ * @Description:
+ * @author:lancs
+ * @date:2020-07-03 14:42
+ * @Copyright:2020 www.jinwang.com.cn Inc. All rights reserved.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
